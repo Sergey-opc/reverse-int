9baae8c951a71d2578684ef08b08482a1c23f944
Volodkevich Sergey <volodkevich777@gmail.com>
2022-07-15 10:38:14 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    let pass = 0;
+    n = Math.abs(n);
+    while (n) {
+        pass = pass * 10 + n % 10;
+        n = Math.floor(n / 10);
+    }
+    return pass;
+}  
